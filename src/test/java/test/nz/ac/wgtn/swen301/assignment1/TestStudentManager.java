771b0e5c50b8c9798dd8dac243210d2c3369c85c
Jens Dietrich <jens.dietrich@gmail.com>
2023-07-18 22:30:42 +1200

    initial commit

---
+++ src/test/java/test/nz/ac/wgtn/swen301/assignment1/TestStudentManager.java
@@ -0,0 +1,27 @@
+package test.nz.ac.wgtn.swen301.assignment1;
+
+import nz.ac.wgtn.swen301.assignment1.StudentManager;
+import nz.ac.wgtn.swen301.studentdb.Student;
+import nz.ac.wgtn.swen301.studentdb.StudentDB;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+public class TestStudentManager {
+
+    // DO NOT REMOVE THE FOLLOWING -- THIS WILL ENSURE THAT THE DATABASE IS AVAILABLE
+    // AND IN ITS INITIAL STATE BEFORE EACH TEST RUNS
+    @BeforeEach
+    public  void init () {
+        StudentDB.init();
+    }
+    // DO NOT REMOVE BLOCK ENDS HERE
+
+    @Test
+    public void dummyTest() throws Exception {
+        Student student = new StudentManager().fetchStudent("id42");
+        // THIS WILL INITIALLY FAIL !!
+        assertNotNull(student);
+    }
+}
